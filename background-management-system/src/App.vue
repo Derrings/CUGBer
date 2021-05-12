7aa7710f06ff1afb40a0bbea45b5142795247b68
Jenny-daling <318184460@qq.com>
2021-05-12 08:34:30 +0800

    页面导航

---
+++ background-management-system/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="app">
+  <div class="slider-nav">
+  <el-row class="tac">
+    <background-index />
+  </el-row>
+  </div>
+</div>
+</template>
+<script>
+import BackgroundIndex from '@/views/BackgroundIndex.vue';
+
+export default {
+  components: {
+    BackgroundIndex,
+  },
+};
+</script>
+<style lang="less">
+  #app{
+    height: 100%;
+    .app{
+      height: 100%;
+      .slider-nav{
+        height: 100%;
+        .tac{
+          height: 100%;
+        }
+      }
+    }
+  }
+</style>
---
+++ background-management-system/src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router';
+// import BackgroundIndex from '@/views/BackgroundIndex.vue';
+
+const routes = [
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   // component: Home,
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  // },
+  // {
+  //   path: '/backgroundIndex',
+  //   name: 'BackgroundIndex',
+  //   component: BackgroundIndex,
+  // }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
